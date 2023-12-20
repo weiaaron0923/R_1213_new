e966de0ad773c53c441448dc44cb88bf49401d0d
吳政緯 <151513619+weiaaron0923@users.noreply.github.com>
2023-12-20 16:47:12 +0800

    ;

--- R/server.R
+++ R/server.R
@@ -222,4 +222,170 @@ shinyServer(function(input, output) {
 })
 
 
+library(shiny)
+shinyServer(function(input, output) {
+  output$summary = renderPrint({
+    if (is.null(input$files)==TRUE) {return("You have to up load your data!!!")} else {
+      dat = read.table(input$files$datapath, header = TRUE)
+      summary(dat)
+    }
+  })
+  output$view = renderTable({
+    if (is.null(input$files)==TRUE) {return()} else {
+      dat = read.table(input$files$datapath, header = TRUE)
+      head(dat, input$n)
+    }
+  })
+})
+
+
+
+
+library(shiny)
+shinyServer(function(input, output) {
+  DATA = reactive({
+    if (is.null(input$files)) {return()} else {
+      dat = read.table(input$files$datapath,header=T)
+      return(dat)
+    }
+  })
+  output$summary = renderPrint({
+    dat = DATA()
+    if (is.null(dat)) {return("You have to up load your data!!!")} else {
+      summary(dat)
+    }
+  })
+  output$scatterPlot = renderPlot({
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      plot(dat,col=input$Color)
+    }
+  })
+  output$view = renderTable({
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      head(dat,input$n)
+    }
+  })
+})
+
+library(shiny)
+shinyServer(function(input, output) {
+  DATA = reactive({
+    if (is.null(input$files)) {return()} else {
+      dat = read.table(input$files$datapath,header=T)
+      return(dat)
+    }
+  })
+  output$scatterPlot = renderPlot({
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      plot(dat,col=input$Color)
+    }
+  })
+  output$download = downloadHandler(
+    filename = function() {'plot.pdf'},
+    content = function(con) {
+      dat = DATA()
+      if (is.null(dat)) {return()} else {
+        pdf(con)
+        plot(dat,col=input$Color)
+        dev.off()
+      }
+    }
+  )
+})
+
+library(shiny)
+shinyServer(function(input, output) {
+  DATA <- reactive({
+    if (is.null(input$files)) {return()} else {
+      dat <- read.table(input$files$datapath,header=T)
+      return(dat)
+    }
+  })
+  output$choose
+  _
+  columns1 <- renderUI({ #這 裡 是 關鍵
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("Y"
+                  , h4("Choose a dependence variable:"), choices = colnames)
+    }
+  })
+  output$choose
+  _
+  co關鍵
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("X"
+                  , h4("Choose a independence variable:"), choices = colnames[which(colnames!=input$Y)])
+    }
+  })
+  output$summary <- renderPrint({
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X] #這 裡 是 關鍵
+      Y <- dat[,input$Y] #這 裡 是 關鍵
+      Result=lm(Y~X)
+      return(summary(Result))
+    }
+  })
+  output$plot <- renderPlot({
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X] #這 裡 是 關鍵
+      Y <- dat[,input$Y] #這 裡 是 關鍵
+      plot(X,Y,pch=19)
+      abline(lm(Y~X),col="black")
+    }
+  })
+})
+
+
+library(shiny)
+shinyServer(function(input, output) {
+  DATA <- reactive({
+    if (is.null(input$files)) {return()} else {
+      dat <- read.table(input$files$datapath,header=T)
+      return(dat)
+    }
+  })
+  output$choose_columns1 <- renderUI({ #這 裡 是 關鍵
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("Y", h4("Choose a dependence variable:"), choices = colnames)
+    }
+  })
+  output$choose_columns關鍵
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)) {return()} else {
+      colnames <- colnames(dat)
+      selectInput("X", h4("Choose a independence variable:"), choices = colnames[which(colnames!=input$Y)])
+    }
+  })
+  output$summary <- renderPrint({
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X] #這 裡 是 關鍵
+      Y <- dat[,input$Y] #這 裡 是 關鍵
+      Result=cor.test(X,Y,method=input$method)
+      return(Result)
+    }
+  })
+  output$plot <- renderPlot({
+    dat = DATA()
+    if (is.null(dat)|is.null(input$Y)|is.null(input$X)) {return()} else {
+      X <- dat[,input$X] #這 裡 是 關鍵
+      Y <- dat[,input$Y] #這 裡 是 關鍵
+      plot(X,Y,pch=19,col=input$Color)
+      abline(lm(Y~X),col="black")
+    }
+  })
+})
+
+
 
--- R/ui.R
+++ R/ui.R
@@ -272,3 +272,117 @@ shinyUI(pageWithSidebar(
 ))
 
 
+
+
+
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Summarize your data"),
+  sidebarPanel(
+    fileInput(inputId="files"
+              , label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file.
+"),
+    sliderInput("n"
+                , h4("Select the observed rows:"), min=5, max=20, value=10)
+  ),
+  mainPanel(
+    verbatimTextOutput("summary"),
+    tableOutput("view")
+  )
+))
+
+
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Summarize your data"),
+  sidebarPanel(
+    fileInput(inputId="files"
+              , label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file.
+"),
+    sliderInput("n"
+                , h4("Select the observed rows:"), min=5, max=20, value=10),
+    radioButtons("Color"
+                 ,
+                 "Select the color of histogram:"
+                 , choices = c("Red" = "red"
+                               ,
+                               "Blue" = "blue"
+                               ,
+                               "Green" = "green"))
+  ),
+  mainPanel(
+    verbatimTextOutput("summary"),
+    plotOutput("scatterPlot"),
+    tableOutput("view")
+  )
+))
+
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Ploting your data"),
+  sidebarPanel(
+    fileInput(inputId="files"
+              , label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file.
+"),
+    radioButtons("Color"
+                 ,
+                 "Select the color of histogram:"
+                 , choices = c("Red" = "red"
+                               ,
+                               "Blue" = "blue"
+                               ,
+                               "Green" = "green"))
+  ),
+  mainPanel(
+    plotOutput("scatterPlot"),
+    downloadButton("download"
+                   , label = "Download plot"
+                   , class = NULL)
+  )
+))
+
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Linear regression for two continuous variables.
+"),
+  sidebarPanel(
+    fileInput(inputId="files"
+              , label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file.
+"),
+    uiOutput("choose
+_
+columns1"), #這 裡 是 關鍵
+    uiOutput("choose
+_
+columns2") #這 裡 是 關鍵
+  ),
+  mainPanel(
+    verbatimTextOutput("summary"),
+    plotOutput("plot"
+               ,width = "500px"
+               , height = "500px")
+  )
+))
+
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Linear regression for two continuous variables."),
+  sidebarPanel(
+    fileInput(inputId="files", label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file."),
+    uiOutput("choose_columns1"), #這 裡 是 關鍵
+    uiOutput("choose_columns2"), #這 裡 是 關鍵
+    radioButtons("method","What is the method to analysis?", choices = c("Pearson correlation" = "pearson","Spearman correlation" ="spearman")),
+    radioButtons("Color","Select the color of histogram:", choices = c("Red" = "red","Blue" = "blue","Green" = "green"))),
+  mainPanel(
+    verbatimTextOutput("summary"),
+    plotOutput("plot",width = "500px", height = "500px")
+  )
+))
+
+
+
